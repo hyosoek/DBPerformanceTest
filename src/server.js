587e990d95fdc942fb497049951e5d3d32da763c
Choi Hyo Seok <tennfin1@gmail.com>
2024-01-12 18:55:11 +0900

    First commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 //clog test
 var bodyParser = require('body-parser')
 
-
-
 //environment setting
 const express =require("express")
 const path = require("path")
@@ -13,11 +11,6 @@ const cookieParser = require('cookie-parser');
 const redis = require("redis").createClient();
 const errorHandler = require("./middleware/errorhandling.js")
 
-const morgan = require("morgan");
-const logger = require("./module/logger");
-const morganFormat = process.env.NODE_ENV !== "production" ? "dev" : combined; // NOTE: morgan 출력 형태 server.env에서 NODE_ENV 설정 production : 배포 dev : 개발
-//const morganFormat = 'combined'; 
-
 const app = express()
 const sslOptions = {
     "key":fs.readFileSync(path.join(__dirname,"../ssl/key.pem")),
@@ -26,7 +19,6 @@ const sslOptions = {
 }
 const server = https.createServer(sslOptions,app)
 
-app.use(morgan(morganFormat, { stream: logger.stream })); // morgan 로그 설정 
 app.use(express.urlencoded( {extended : true } ));
 app.use(express.json()) // 타입 변환(req에 대한 body-parser)
 app.use(express.static('../'));
@@ -38,7 +30,6 @@ app.use(cors({
     credentials: true
   }));
 
-//clog 테스트용
 app.get("*",(req,res,next) =>{
     const protocol = req.protocol 
     if(protocol == "https"){
@@ -50,22 +41,12 @@ app.get("*",(req,res,next) =>{
 })
 
 //API
-app.use("/account", require("./router/account"))
-app.use("/main", require("./router/main"))
-app.use("/appliance", require("./router/appliance"))
-app.use("/nodam", require("./router/Nodam"))
-app.use("/auth", require("./router/Clog"))
-app.use("/auth", require("./router/Clog2"))
-
-// const postApi = require("./router/appliance")
-// app.use("/appliance",postApi) 
-// const commentApi = require("./router/recommend")
-// app.use("/recommend",commentApi) 
+app.use("/account", require("./router/postgresql"))
+app.use("/account", require("./router/mariadb"))
 
 app.use(function (err, req, res, next) {
     errorHandler.errorPass(err,req,res)
 });
-
 app.use(errorHandler.error404Pass);
 
 //서버 시작
