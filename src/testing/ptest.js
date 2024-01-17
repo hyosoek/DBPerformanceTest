4a90a20d8eabfa0b37650e91779cbc258103c4e8
Choi Hyo Seok <tennfin1@gmail.com>
2024-01-17 20:59:20 +0900

    Make main function to syncro..

--- src/testing/ptest.js
+++ src/testing/ptest.js
@@ -6,9 +6,22 @@ const client = require("../../config/database.js");
 
 class postgreTestClass {
     constructor () {
-        console.log("Under this line is Ptest Latency")
         // https://kicksky.tistory.com/29
+        this.mainFunction()  
+    }
 
+    mainFunction = async(sql,count) => {
+        console.log("Pk - B-tree")
+        const sql0 = `EXPLAIN ANALYZE 
+                        SELECT 
+                            *
+                        FROM 
+                            account 
+                        WHERE 
+                            id=5000000;`
+        await this.showAverageSelectLatency(sql0,20)
+
+        console.log("Hash idx")
         const sql1 = `EXPLAIN ANALYZE 
                         SELECT 
                             *
@@ -16,40 +29,44 @@ class postgreTestClass {
                             account 
                         WHERE 
                             mail = 'tennfin1@gmail.com1';`
-        this.showAverageSelectLatency(sql1,10)
-
-        // const sql2 = `EXPLAIN ANALYZE SELECT 
-        //                     count(*) 
-        //                 FROM account`
-        // this.showAverageSelectLatency(sql)  
+        await this.showAverageSelectLatency(sql1,20)
+    
+        console.log("Non idx")
+        const sql2 = `EXPLAIN ANALYZE 
+                        SELECT 
+                            *
+                        FROM 
+                            account
+                        WHERE
+                            longitude=-48.35523039348061;`
+        await this.showAverageSelectLatency(sql2,20)
     }
 
     showAverageSelectLatency = async(sql,count) => {
-        const repeatCount = count;
-        let sum = 0;
-        const latencyVector = [];
-        for (let i = 0 ; i < repeatCount ; i++){
-            latencyVector.push(await this.showSelectLatency(sql))
+        let sum = 0
+        const latencyVector = []
+        for (let i = 0 ; i < count ; i++){
+            const latencyTemp = await this.showSelectLatency(sql)
+            latencyVector.push(latencyTemp)
         }
         latencyVector.sort()
 
         for(let i = 1; i < latencyVector.length -1 ; i++){
             sum += latencyVector[i]
         }
-        
-        console.log("Average Latency(without min & max) : " + sum/(repeatCount-2) + " ms");
+        console.log("Average Latency(without min & max) : " + sum/(count-2) + " ms");
     }
 
     showSelectLatency = async(sql) =>{
-        let returnValue;
+        let returnValue
         const data = await client.query(sql)
         const row = data.rows
         const valueTemp = (row[row.length-1])
 
         returnValue = parseFloat(valueTemp['QUERY PLAN'].match(/(\d+\.\d+)/)[0]);
         
-        
-        return console.log(returnValue)
+        console.log(returnValue)
+        return returnValue
     }
 }
 
